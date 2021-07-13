0fd18f345a76194393812fbe78b98e6d00e32da8
luckpoy <986827989@qq.com>
2021-07-13 21:01:46 +0800

    vt

--- leecode/164.cpp
+++ leecode/164.cpp
@@ -2,18 +2,48 @@
 #include <vector>
 using namespace std;
 
-int maximumGap(vector<int>& nums) {
-    int i, j, temp;
-	for (j = 0; j < nums.size() - 1; j++)
+void myswap(int &a ,int &b){
+    int temp = a;
+    a=b;
+    b=temp;
+}
+
+void quickSort(int left, int right, vector<int>& arr)
+{
+	if(left >= right)
+		return;
+	int i, j, base, temp;
+	i = left, j = right;
+	base = arr[left];  //取最左边的数为基准数
+	while (i < j)
 	{
-		for (i = 0; i < nums.size() - 1 - j; i++)
-		if (nums[i] > nums[i + 1])
+		while (arr[j] >= base && i < j)
+			j--;
+		while (arr[i] <= base && i < j)
+			i++;
+		if(i < j)
 		{
-			temp = nums[i];
-			nums[i] = nums[i + 1];
-			nums[i + 1] = temp;
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
 		}
 	}
+	//基准数归位
+	arr[left] = arr[i];
+	arr[i] = base;
+	quickSort(left, i - 1, arr);//递归左边
+	quickSort(i + 1, right, arr);//递归右边
+
+}
+int maximumGap(vector<int>& nums) {
+	
+    // for(int i =0; i<nums.size();i++){
+        
+    //     if(nums[i]>nums[nums.size()-1]){
+    //         myswap(nums[i],nums[nums.size()-1]);
+    //     }
+    //  }
+    quickSort(0,nums.size()-1,nums);
 
     int maxerror=0;
     for(int k=0;k<nums.size()-1;k++){
@@ -24,8 +54,8 @@ int maximumGap(vector<int>& nums) {
 
 int main(int argc, char const *argv[])
 {
-    int b[7]={1,6,3,23,5,6,0};
-    vector<int> a(b,b+7);
+    int b[4]={100,3,2,1};
+    vector<int> a(b,b+4);
     cout<<maximumGap(a)<<endl;
     return 0;
 }
